3da7ea331a2ebd06b6c5caa9f0d1515d5ef37f2a
Gabor Sera <gabor@designatives.com>
2023-06-20 10:30:06 +0200

    test new robot

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,14 @@ module.exports = {
 			},
 		},
 		{
+			resolve: 'gatsby-plugin-robots-txt',
+			options: {
+				//host: 'https://www.example.com',
+				//sitemap: 'https://www.example.com/sitemap.xml',
+				policy: [{userAgent: '*', allow: '/'}]
+			}
+		},
+		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `brands`,
