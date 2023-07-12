c1a88a278be07f3457209be6ce7191c47e126ce9
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-07-12 15:00:14 -0400

    fix: typo

--- src/components/sections/days-agenda.js
+++ src/components/sections/days-agenda.js
@@ -37,7 +37,7 @@ export default function DaysAgenda({ data }) {
 												fill='#4F02EC'
 											/>
 										</svg>
-										<div className="button-label">Link is comming soon</div>
+										<div className="button-label">Link is coming soon</div>
 									</div>
 								</div>
 							</div>
