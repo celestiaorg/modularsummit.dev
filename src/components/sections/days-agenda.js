a252367ed059502ea0de883ed893e7d5f053ab8f
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-07-13 23:54:14 -0400

    cleanup & audit
    - hide "Speakers:" if there are none
    - schedule swaps
    - add Josh & Diego to site
    - hide "ZK Track" and "Gaming" tags that show on homepage (because they are hidden on Day 1 and Day 2 views)

--- src/components/sections/days-agenda.js
+++ src/components/sections/days-agenda.js
@@ -94,10 +94,12 @@ function EventItem({ index, item }) {
 					</div>
 					<div className='event-text w-[100px]'>{item.time}</div>
 				</div>
+				{item.speakers && (
 				<div className='flex items-start basis-full md:basis-auto'>
 					<div className='min-w-[100px] md:min-w-[110px] lg:min-w-[135px] event-text'>Speakers:</div>
 					<div className='w-full event-text event-text-speakers'>{item.speakers}</div>
 				</div>
+				)}
 			</div>
 		</li>
 	);
